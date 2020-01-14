947ca7ac3f9faa63579f373ca31dad2aeaea9a3d
Ivan <inovoafst@gmail.com>
2020-01-14 12:32:51 +0100

    navbar-footer-envBBDD

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,11 @@
+<!-- Footer -->
+<footer class="page-footer font-small blue">
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Copyright Aula DAW:
+    <a href="https://mdbootstrap.com/education/bootstrap/"> Clase DAW2</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
